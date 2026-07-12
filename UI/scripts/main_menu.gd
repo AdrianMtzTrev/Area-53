3c672c08324ca7d38bfee29343ca07cb8c7c9327
Jesus Leal <jisusleal15@gmail.com>
2026-07-11 19:10:30 -0600

    Correccion start

--- UI/scripts/main_menu.gd
+++ UI/scripts/main_menu.gd
@@ -4,7 +4,7 @@ class_name Main_Menu
 
 
 func _on_start_bttn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Objects/escena_test.tscn")
+	get_tree().change_scene_to_file("res://assets/Objects/escena_test.tscn")
 
 
 func _on_exit_bttn_pressed() -> void:

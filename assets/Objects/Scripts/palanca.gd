875c7d16968764deda9b067c9e9b5a4a9f8ee8c8
Jesus Leal <jisusleal15@gmail.com>
2026-07-11 21:53:16 -0600

    Puzzle Fusibles completado

--- assets/Objects/Scripts/palanca.gd
+++ assets/Objects/Scripts/palanca.gd
@@ -5,17 +5,33 @@ extends StaticBody3D
 
 var esta_bajada = false
 
+@onready var foco_led = $FocoLED
+#@onready var mesh_palanca = $MeshPalanca
+
+func _ready():
+	cambiar_color_led(Color(1,0,0))
+
 func iniciar_puzzle():
 	esta_bajada = !esta_bajada
 	
 	if esta_bajada:
-		rotation_degrees.x = -45
+		cambiar_color_led(Color(0,1,0))
 	else:
-		rotation_degrees.x = 0
+		cambiar_color_led(Color(1,0,0))
 		
 	if cerebro_puzzle:
 		cerebro_puzzle.comprobar_palanca(numero_palanca, esta_bajada)
 		
 func reiniciar():
 	esta_bajada = false
-	rotation_degrees.x = 0
+	cambiar_color_led(Color(1,0,0))
+	
+func cambiar_color_led(color_nuevo: Color):
+	var material_led = StandardMaterial3D.new()
+	material_led.albedo_color = color_nuevo
+	
+	material_led.emission_enabled = true
+	material_led.emission = color_nuevo
+	material_led.emission_energy_multiplier = 2.0
+	
+	foco_led.material_override = material_led
